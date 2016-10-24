0463236cc076545ae117d867200348d8f8d3cdf2
22222 <22222@users.noreply.github.com>
2016-10-23 19:59:00 -0500

    Add appveyor and nuget config files.

--- GogsKit/Properties/AssemblyInfo.cs
+++ GogsKit/Properties/AssemblyInfo.cs
@@ -9,4 +9,4 @@ using System.Runtime.InteropServices;
 [assembly: ComVisible(false)]
 [assembly: Guid("7d74786b-d001-4c6b-b43e-e2df98c51e03")]
 
-[assembly: AssemblyVersion("0.4.0.*")]
+[assembly: AssemblyVersion("0.0.0.*")]
